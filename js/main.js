beeb6b56763fbf497b3416967607549be0bfe8d0
Lapis <88260244+LapisPhoenix@users.noreply.github.com>
2024-02-16 20:37:46 -0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ fetchLatestBlogs = () => {
     fetch(url)
         .then(resp => resp.json())
         .then(files => {
-            const message = files.message();
+            const message = files.message;
             if (message) {
                 if (message === "This repository is empty." || message === "Not Found") {
                     // Let the user know that there are no blogs
@@ -15,14 +15,34 @@ fetchLatestBlogs = () => {
 
             files.forEach(file => {
                 const blogUrl = file.download_url;
+                const blogName = file.name;
+
+                if (blogName.startsWith("IGNORE_")) {
+                    // Skip this blog
+                    return;
+                }
                 fetch(blogUrl)
                     .then(resp => resp.text())
                     .then(blog => {
-                        // Do something with the blog
-                        console.log(blog);
+                        // Line 1 is the title
+                        const title = blog.split("\n")[0].trim().split("title: ")[1];
+                        // Line 2 is the description
+                        const description = blog.split("\n")[1].trim().split("description: ")[1];
+                        // Line 3 is the date
+                        const date = blog.split("\n")[2].trim().split("date: ")[1];
+                        // The rest is the content
+                        const content = blog.split("\n").slice(3).join("\n").trim();
+
+                        // For now just print the blog
+                        console.log(title);
+                        console.log(description);
+                        console.log(date);
+                        console.log(content);
                     })
                     .catch(err => console.error(err));
             })
         })
         .catch(err => console.error(err));
 };
+
+fetchLatestBlogs();
---
+++ pages/test.md
@@ -0,0 +1,44 @@
+title: test blog title
+description: test blog description
+date: 2-16-2024
+
+# Test
+This is a test blog post.
+
+![test image](../images/placeholder.jpg)
+
+```python
+print("Hello, World!")
+```
+
+- [ ] Test
+- [x] Test
+
+
+# Header 1
+## Header 2
+### Header 3
+#### Header 4
+##### Header 5
+###### Header 6
+
+**Bold Text**
+
+*Italic Text*
+
+[Link](https://www.google.com)
+
+> Blockquote
+> Blockquote
+> Blockquote
+
+1. List
+2. List
+3. List
+
+- List
+- List
+  - List
+    - List
+
+---
